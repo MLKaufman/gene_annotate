b94facb6c5a5011ad2acc0de3b48f1e4c7c98f0f
Michael Kaufman <MLKaufman@users.noreply.github.com>
2021-08-16 14:54:39 -0600

    zip safe

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     name='gene_annotate',
     version='1.0,
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
-    include_package_data=True
+    include_package_data=True,
+    zip_safe=False,
     entry_points={'console_scripts': ['gene_annotate=gene_annotate.gene_annotate:main']},
     python_requires=">=3.6"
 )
