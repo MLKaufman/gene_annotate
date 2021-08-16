9d0a5977cd2421da024e88277a9632ee18c23a76
Michael Kaufman <MLKaufman@users.noreply.github.com>
2021-08-16 14:27:30 -0600

    changes

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
     name='gene_annotate',
     version='1.0,
     entry_points={
-        'console_scripts': ['gene_annotate=gene_annotate.gene_annotate:main']
-    }
+    'console_scripts': ['gene_annotate=gene_annotate.gene_annotate:main']}
 )
