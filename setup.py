bd0eb3a4970e68ae2f8e8a59c5a80b2f2c22e786
Michael Kaufman <MLKaufman@users.noreply.github.com>
2021-08-17 19:21:31 -0600

    back to package data

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ setup(
     name='gene_annotate',
     version='1.0',
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
-    include_package_data=True,
     entry_points={'console_scripts': ['gene_annotate=gene_annotate.gene_annotate:main']},
     python_requires=">=3.6",
-    install_requires="pandas"
-    #package_data={'gene_annotate': ['/databases/*.csv']}
+    install_requires="pandas",
+    package_data={'gene_annotate': ['/databases/*.csv']}
 )
