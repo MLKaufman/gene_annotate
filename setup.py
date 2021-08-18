466d7d2a0258ceb0d108d367cbe3afab9bf47027
Michael Kaufman <MLKaufman@users.noreply.github.com>
2021-08-17 19:05:25 -0600

    remove pkgresurces

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     zip_safe=False,
     entry_points={'console_scripts': ['gene_annotate=gene_annotate.gene_annotate:main']},
     python_requires=">=3.6",
-    install_requires="pandas", "pkg-resources",
+    install_requires="pandas",
     package_data={'gene_annotate': ['/databases/*.csv']}
 )
